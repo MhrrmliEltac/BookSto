64ae6b90a099511554dadf79152652bd4d30fdf6
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-02-04 23:53:38 +0400

    Comment add update

--- src/components/book-detail/BookDetail.tsx
+++ src/components/book-detail/BookDetail.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
-import { addComment, fetchDocumentsByCondition } from "../../../utils/firebase";
+import {
+  addComment,
+  addToCart,
+  addToFavourites,
+  fetchDocumentsByCondition,
+} from "../../../utils/firebase";
 import { useParams } from "react-router";
 import Heading from "../general/Heading";
 import ShortenedText from "../general/ShortenedText";
@@ -8,6 +13,8 @@ import BasicButton from "../general/Button";
 import { MdFavorite } from "react-icons/md";
 import toast from "react-hot-toast";
 import Comments from "./Comments";
+import { useAppDispatch, useAppSelector } from "../../hook/hooks";
+import { fetchCartLength } from "../redux/slice/count";
 
 interface Review {
   comment: string[];
@@ -23,15 +30,36 @@ interface Book {
   review?: Review;
 }
 
+type ActionFnType = (userId: string, book: any) => Promise<void>;
+
 const BookDetail = () => {
   const [bookDetails, setBookDetails] = useState<object[] | undefined>(
     undefined
   );
   const [textArea, setTextArea] = useState<string>();
   const [bookCommentState, setBookComment] = useState<any>();
+  const [isToken, setIsToken] = useState<boolean>(false);
+  const user = useAppSelector((state: any) => state.auth.user);
+  const dispatch = useAppDispatch();
 
   let params = useParams();
   let bookComment: string[] = [];
+
+  const handleAction = async (actionFn: ActionFnType, book: any) => {
+    if (isToken) {
+      try {
+        await actionFn(user?.user.uid, book);
+        if (actionFn === addToCart) {
+          dispatch(fetchCartLength(user?.user.uid));
+        }
+      } catch (error) {
+        console.error("Error in handleAction:", error);
+        toast.error("Something went wrong. Please try again.");
+      }
+    } else {
+      toast.error("Please, sign in or sign up");
+    }
+  };
 
   const fetchBookToId = async (id: string | undefined) => {
     const book = await fetchDocumentsByCondition("books", Number(id));
@@ -53,12 +81,18 @@ const BookDetail = () => {
       await addComment({ comment: bookComment }, id);
       toast.success("Comment added successfully");
       bookComment.pop();
+    } else {
+      toast.error("Text area is empty");
     }
   };
 
   useEffect(() => {
     fetchBookToId(params.id);
   }, [textArea, addCommentToReview]);
+
+  useEffect(() => {
+    setIsToken(!!(user && user.user?.providerData.length > 0));
+  }, [user]);
 
   return (
     <div className="container">
@@ -94,12 +128,18 @@ const BookDetail = () => {
                     </p>
                   </div>
                   <div className="flex gap-4">
-                    <BasicButton text="Add To Cart" />
+                    <BasicButton
+                      text="Add To Cart"
+                      onClick={() => handleAction(addToCart, detail)}
+                    />
                     <BasicButton text="Read Sample" white />
                   </div>
-                  <div className="flex gap-2 items-center mt-4 ">
+                  <div className="flex gap-2 items-center mt-4">
                     <div className="bg-[#0DD6B8] flex items-center justify-center py-2 px-2 rounded-full cursor-pointer">
-                      <MdFavorite className="text-white" />
+                      <MdFavorite
+                        className="text-white"
+                        onClick={() => handleAction(addToFavourites, detail)}
+                      />
                     </div>
                     <p className="mb-0 text-md text-[#C7D7E2]">
                       Add to Wishlist{" "}
@@ -131,8 +171,8 @@ const BookDetail = () => {
       <div className="flex flex-col rounded-md bg-white p-4 mt-10">
         <Heading text="Review" />
         {bookCommentState &&
-          bookCommentState.map((comment: string) => (
-            <Comments comment={comment} />
+          bookCommentState.map((comment: string, index: number) => (
+            <Comments comment={comment} key={index} />
           ))}
       </div>
     </div>
